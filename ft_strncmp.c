5a41d651239de88f2146547d6314544af36f1cd3
Chr-ss <138532773+Chr-ss@users.noreply.github.com>
2023-10-20 08:48:35 +0200

    Update ft_strncmp.c

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -17,11 +17,13 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	int	i;
 
 	i = 0;
-	while (n-- && s1[i] && s2[i])
+	while (n--)
 	{
 		if (s1[i] != s2[i])
 			return ((unsigned char) s1[i] - (unsigned char) s2[i]);
 		i++;
+		if (!s1[i] && !s2[i])
+			break ;
 	}
 	return (0);
 }
