b0dfbed989040948831bfae732976020e881e4d6
Chr-ss <138532773+Chr-ss@users.noreply.github.com>
2023-10-20 08:44:19 +0200

    Update ft_memcpy.c

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -19,6 +19,8 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 
 	d = (unsigned char *) dest;
 	s = (unsigned const char *) src;
+	if (!dest || !src)
+		return (dest);
 	while (n--)
 		*d++ = *s++;
 	return (dest);
